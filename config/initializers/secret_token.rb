3bb7e240da7e788694e963baeba0806257f8d477
Matt Schallert <mattschallert@gmail.com>
2013-02-04 18:00:57 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SigepFines::Application.config.secret_token = 'a6ed9dbb7c245ac6ca98ae47bf8a6ca8c046bcb027d2ab0de8bcaea7eaf362a56cc2bdbe6cc31d7bfd7ef4bb98bf134705d84633a73a9a50ca910ddcc24f7d33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SigepFines::Application.config.session_store :cookie_store, key: '_SigepFines_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SigepFines::Application.config.session_store :active_record_store
