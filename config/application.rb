e0cdcf90678223aaedae3778bb9738d5527f0b81
Matt Schallert <mattschallert@gmail.com>
2013-02-04 21:36:37 -0500

    Installed Thin and Devise, installed configs
    Made sample app config, load real config in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+CONFIG.merge! CONFIG.fetch(Rails.env, {})
+CONFIG.symbolize_keys!
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
