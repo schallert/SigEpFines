3acb5e8926540cceff1e01ab4ba01f8dbc2e1b6f
Matt Schallert <mattschallert@gmail.com>
2013-02-04 22:01:35 -0500

    Added model relations

--- app/models/brother.rb
+++ app/models/brother.rb
@@ -1,3 +1,5 @@
 class Brother < ActiveRecord::Base
   attr_accessible :email, :name, :phone_number
+
+  has_many :fines
 end
--- app/models/fine.rb
+++ app/models/fine.rb
@@ -1,3 +1,5 @@
 class Fine < ActiveRecord::Base
   attr_accessible :amount, :brother_id, :date_assigned
+
+  has_one :brother
 end
