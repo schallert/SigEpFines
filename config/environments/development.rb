1c3bb7f84f46c05ec4c2bc26eb12e87cb16b1a4e
Matt Schallert <mattschallert@gmail.com>
2013-04-01 12:25:21 -0400

    Committed debug info
    Because not committing this shit is a fucking pain in the ass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,22 @@ SigepFines::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  class DisableAssetsLogger
+    def initialize(app)
+      @app = app
+      Rails.application.assets.logger = Logger.new('/dev/null')
+    end
+
+    def call(env)
+      previous_level = Rails.logger.level
+      Rails.logger.level = Logger::ERROR if env['PATH_INFO'].index("/assets/") == 0
+      @app.call(env)
+    ensure
+      Rails.logger.level = previous_level
+    end
+  end
+
+  config.middleware.insert_before Rails::Rack::Logger, DisableAssetsLogger
+  
 end
